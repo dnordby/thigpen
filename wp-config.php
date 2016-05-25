3dedc6817dd0432e57e25b3145e629821975f48a
Daniel Nordby <nordbydaniel@gmail.com>
2016-05-24 22:39:39 -0400

    updates

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'ryanthigpen_danielnordby');
+define('DB_NAME', 'ryanthigpen');
 
 /** MySQL database username */
-define('DB_USER', 'ryanthigpendanie');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'NdDtRDYK');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql.ryanthigpen.danielnordby.nyc');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
