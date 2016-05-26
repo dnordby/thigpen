698bc3149b9543beb93295b1a18c19a0891ca54b
Daniel Nordby <nordbydaniel@gmail.com>
2016-05-25 22:01:30 -0400

    initial landing page

---
+++ wp-content/themes/thigpen/templates/landing.php
@@ -0,0 +1,9 @@
+<?php /* Template Name: Landing */ ?>
+<div class="tile-row row">
+	<div class="tile-wrapper col-md-5 no-pad">
+		<div class="tile one col-xs-12 col-sm-6 no-pad"></div>
+		<div class="tile two col-xs-12 col-sm-6 no-pad"></div>
+		<div class="tile three col-xs-12 col-sm-6 no-pad"></div>
+		<div class="tile four col-xs-12 col-sm-6 no-pad"></div>
+	</div>
+</div>
